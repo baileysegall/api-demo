27d22bad01f9a1470fa74a2898c69e044e07055c
Rebecca Dang <ph.rdang@gmail.com>
2021-10-23 09:48:44 -0700

    Format with Prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-//Necessary import statements 
-import './App.css';
+//Necessary import statements
+import "./App.css";
 import axios from "axios";
-import React, { useState }  from 'react';
+import React, { useState } from "react";
 
 //The App function is the main output function which all the main code is stored in
 // in a more complex application may have multiple components used together
@@ -22,29 +22,27 @@ function App() {
   //Handles getting more info for the hover
   function moreInfo(doggo) {
     //checks if the breeds table exists within the dog object
-    if(doggo[0].breeds[0]){
+    if (doggo[0].breeds[0]) {
       //if the table exists display the name in the hover
-      return (
-        <div class="text">{doggo[0].breeds[0].name}</div>
-      )
-    } else{
+      return <div class="text">{doggo[0].breeds[0].name}</div>;
+    } else {
       //if the table doesn't exist.. display no information
-      return (
-        <div class="text">No Information</div>
-      )
+      return <div class="text">No Information</div>;
     }
   }
 
-
   //If doggo is not updated (the button has not been clicked) return no image
-  if(doggo === null){
+  if (doggo === null) {
     return (
       <div class="main">
         <p class="title">CODEBASE API DEMO</p>
         {/* when button is called called url function */}
-        <button onClick={getDogWithAxios} class="button" >Gimme Doggies</button>
+        <button onClick={getDogWithAxios} class="button">
+          Gimme Doggies
+        </button>
       </div>
-  );}
+    );
+  }
   //if the doggo is updated render the image and the hover component
   else {
     return (
@@ -52,16 +50,18 @@ function App() {
         <p class="title-with-image">CODEBASE API DEMO</p>
         <div class="container">
           {/* Render image from url */}
-          <img class="image" src= {doggo[0].url} alt="doggo"/>
+          <img class="image" src={doggo[0].url} alt="doggo" />
           <div class="overlay">
             {/* calls more information to display  */}
             <div class="text">{moreInfo(doggo)}</div>
           </div>
         </div>
-        
-        <button onClick={getDogWithAxios} class="button-with-image" >Gimme Doggies</button>
+
+        <button onClick={getDogWithAxios} class="button-with-image">
+          Gimme Doggies
+        </button>
       </div>
-  );
+    );
   }
 }
 //exports the app
