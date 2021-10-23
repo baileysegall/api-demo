1cbf9e54eb12c6f87656fea9cdfea1731e3b4b3a
Bailey Segall <aquab@segallgirls.com>
2021-10-23 05:18:45 -0700

    just comments

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
+//Necessary import statements 
 import './App.css';
 import axios from "axios";
-import React, { useState, useEffect }  from 'react';
+import React, { useState }  from 'react';
 
+//The App function is the main output function which all the main code is stored in
+// in a more complex application may have multiple components used together
 function App() {
+  //if we use let the variable will not persist within internal functions
+  //initialize as null
   const [doggo, SetDoggo] = useState(null);
-  //const apiURL = "https://api.thedogapi.com/v1/images/search?breed_id=10&limit=12%5C";
+  //link api link -> every time called givees a new object
   const apiURL = "https://api.thedogapi.com/v1/images/search";
 
+  //Calls api linke (gets new object) sets Doggo to new object
   const getDogWithAxios = async () => {
+    //async function allows us not to excede depth my calling function constantly as with useEffect
     const response = await axios.get(apiURL);
     SetDoggo(response.data);
   };
 
+  //Handles getting more info for the hover
   function moreInfo(doggo) {
+    //checks if the breeds table exists within the dog object
     if(doggo[0].breeds[0]){
-      console.log('yes')
-      console.log(doggo[0].breeds[0].name)
+      //if the table exists display the name in the hover
       return (
         <div class="text">{doggo[0].breeds[0].name}</div>
       )
     } else{
-      console.log('no')
+      //if the table doesn't exist.. display no information
       return (
         <div class="text">No Information</div>
       )
@@ -28,20 +36,25 @@ function App() {
   }
 
 
+  //If doggo is not updated (the button has not been clicked) return no image
   if(doggo === null){
     return (
       <div class="main">
         <p class="title">CODEBASE API DEMO</p>
+        {/* when button is called called url function */}
         <button onClick={getDogWithAxios} class="button" >Gimme Doggies</button>
       </div>
   );}
+  //if the doggo is updated render the image and the hover component
   else {
     return (
       <div class="main">
         <p class="title-with-image">CODEBASE API DEMO</p>
         <div class="container">
+          {/* Render image from url */}
           <img class="image" src= {doggo[0].url} alt="doggo"/>
           <div class="overlay">
+            {/* calls more information to display  */}
             <div class="text">{moreInfo(doggo)}</div>
           </div>
         </div>
@@ -51,5 +64,5 @@ function App() {
   );
   }
 }
-
+//exports the app
 export default App;
