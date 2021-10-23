e2f6c47e6ea6c77b6b331b894fdb486d0269e5a1
Bailey Segall <aquab@segallgirls.com>
2021-10-23 05:00:35 -0700

    Basic Front end and API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+import React, { useState, useEffect }  from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [doggo, SetDoggo] = useState(null);
+  //const apiURL = "https://api.thedogapi.com/v1/images/search?breed_id=10&limit=12%5C";
+  const apiURL = "https://api.thedogapi.com/v1/images/search";
+
+  const getDogWithAxios = async () => {
+    const response = await axios.get(apiURL);
+    SetDoggo(response.data);
+  };
+
+  function moreInfo(doggo) {
+    if(doggo[0].breeds[0]){
+      console.log('yes')
+      console.log(doggo[0].breeds[0].name)
+      return (
+        <div class="text">{doggo[0].breeds[0].name}</div>
+      )
+    } else{
+      console.log('no')
+      return (
+        <div class="text">No Information</div>
+      )
+    }
+  }
+
+
+  if(doggo === null){
+    return (
+      <div class="main">
+        <p class="title">CODEBASE API DEMO</p>
+        <button onClick={getDogWithAxios} class="button" >Gimme Doggies</button>
+      </div>
+  );}
+  else {
+    return (
+      <div class="main">
+        <p class="title-with-image">CODEBASE API DEMO</p>
+        <div class="container">
+          <img class="image" src= {doggo[0].url} alt="doggo"/>
+          <div class="overlay">
+            <div class="text">{moreInfo(doggo)}</div>
+          </div>
+        </div>
+        
+        <button onClick={getDogWithAxios} class="button-with-image" >Gimme Doggies</button>
+      </div>
   );
+  }
 }
 
 export default App;
